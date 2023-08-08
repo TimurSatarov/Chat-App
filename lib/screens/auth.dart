3490e56df109bc5fb706f0cd5ff2da7ab407960f
Timur <timursatarov3@gmail.com>
2023-08-08 16:05:50 +0200

    first commit

---
+++ lib/screens/auth.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final _firebase = FirebaseAuth.instance; // Initialize Firebase Authentication
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  final _form = GlobalKey<FormState>(); // Key to access the form state
+  var _enteredEmail = '';
+  var _enteredPassword = '';
+  var _isLogin =
+      true; // A flag to track whether user is in login or sign-up mode
+
+  void _submit() async {
+    final isValid = _form.currentState!.validate(); // Validate form fields
+    if (!isValid) {
+      return; // If form is not valid, stop processing
+    }
+    _form.currentState!.save(); // Save the form fields' values
+
+    try {
+      if (_isLogin) {
+        // Log user in (to be implemented)
+        final userCredential = await _firebase.signInWithEmailAndPassword(
+          email: _enteredEmail,
+          password: _enteredPassword,
+        );
+        print(userCredential);
+      } else {
+        final userCredentials = await _firebase.createUserWithEmailAndPassword(
+          email: _enteredEmail,
+          password: _enteredPassword,
+        );
+        print(userCredentials); // Print user registration details
+      }
+    } on FirebaseAuthException catch (error) {
+      // Handle Firebase authentication exceptions
+      if (error.code == 'email-already-in-use') {
+        // Handle case where email is already in use
+      }
+      ScaffoldMessenger.of(context)
+          .clearSnackBars(); // Clear any existing snack bars
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(error.message ?? "Authentication failed."),
+        ),
+      ); // Show an error message to the user using a snack bar
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(
+                  top: 30,
+                  bottom: 20,
+                  left: 20,
+                  right: 20,
+                ),
+                width: 200,
+                child: Image.asset('assets/images/chat.png'),
+              ),
+              Card(
+                margin: const EdgeInsets.all(20),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Form(
+                      key: _form, // Associate the key with the form
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: 'Email Address',
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                            autocorrect: false,
+                            textCapitalization: TextCapitalization.none,
+                            validator: (value) {
+                              if (value == null ||
+                                  value.trim().isEmpty ||
+                                  !value.contains('@')) {
+                                return 'Please enter a valid email address';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              _enteredEmail = value!;
+                            },
+                          ),
+                          TextFormField(
+                            decoration:
+                                const InputDecoration(labelText: 'Password'),
+                            validator: (value) {
+                              if (value == null || value.trim().length < 8) {
+                                return 'Password must be at least 8 characters long.';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              _enteredPassword = value!;
+                            },
+                            obscureText: true,
+                          ),
+                          const SizedBox(height: 12),
+                          ElevatedButton(
+                            onPressed:
+                                _submit, // Call the _submit function on button press
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(context)
+                                  .colorScheme
+                                  .primaryContainer,
+                            ),
+                            child: Text(_isLogin ? "Login" : "Sign-up"),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                _isLogin =
+                                    !_isLogin; // Toggle between login and sign-up modes
+                              });
+                            },
+                            child: Text(_isLogin
+                                ? "Create an account"
+                                : "I already have an account. Login"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
