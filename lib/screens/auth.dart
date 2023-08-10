a938293513582430993bee5fc73190bd391abb87
Timur <timursatarov3@gmail.com>
2023-08-10 23:30:04 +0200

    initial commit

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,4 +1,5 @@
 import 'package:chat_app/widgets/user_image_picker.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -17,7 +18,9 @@ class _AuthScreenState extends State<AuthScreen> {
   final _form = GlobalKey<FormState>(); // Key to access the form state
   var _enteredEmail = '';
   var _enteredPassword = '';
+  var _enteredUsername = '';
   var _isLogin = true;
+  var _isAuthenticating = false;
   File? _selectedImage;
 
   void _submit() async {
@@ -28,12 +31,16 @@ class _AuthScreenState extends State<AuthScreen> {
     _form.currentState!.save(); // Save the form fields' values
 
     try {
+      setState(() {
+        _isAuthenticating = true;
+      });
       if (_isLogin) {
         // Log user in (to be implemented)
         final userCredentials = await _firebase.signInWithEmailAndPassword(
           email: _enteredEmail,
           password: _enteredPassword,
         );
+        print(userCredentials);
       } else {
         final userCredentials = await _firebase.createUserWithEmailAndPassword(
           email: _enteredEmail,
@@ -45,6 +52,15 @@ class _AuthScreenState extends State<AuthScreen> {
             .child('${userCredentials.user!.uid}.jpg');
         await storageRef.putFile(_selectedImage!);
         final imageUrl = await storageRef.getDownloadURL();
+
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(userCredentials.user!.uid)
+            .set({
+          'username': _enteredUsername,
+          'email': _enteredEmail,
+          'image_url': imageUrl,
+        });
       }
     } on FirebaseAuthException catch (error) {
       // Handle Firebase authentication exceptions
@@ -58,6 +74,9 @@ class _AuthScreenState extends State<AuthScreen> {
           content: Text(error.message ?? "Authentication failed."),
         ),
       ); // Show an error message to the user using a snack bar
+      setState(() {
+        _isAuthenticating = false;
+      });
     }
   }
 
@@ -117,40 +136,61 @@ class _AuthScreenState extends State<AuthScreen> {
                           ),
                           TextFormField(
                             decoration:
-                                const InputDecoration(labelText: 'Password'),
+                                const InputDecoration(labelText: 'Username'),
+                            enableSuggestions: false,
                             validator: (value) {
-                              if (value == null || value.trim().length < 8) {
-                                return 'Password must be at least 8 characters long.';
+                              if (value == null ||
+                                  value.isEmpty ||
+                                  value.trim().length < 4) {
+                                return "Please enter at least 4 characters";
                               }
                               return null;
                             },
                             onSaved: (value) {
-                              _enteredPassword = value!;
+                              _enteredUsername = value!;
                             },
-                            obscureText: true,
                           ),
-                          const SizedBox(height: 12),
-                          ElevatedButton(
-                            onPressed:
-                                _submit, // Call the _submit function on button press
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Theme.of(context)
-                                  .colorScheme
-                                  .primaryContainer,
+                          if (!_isLogin)
+                            TextFormField(
+                              decoration:
+                                  const InputDecoration(labelText: 'Password'),
+                              validator: (value) {
+                                if (value == null || value.trim().length < 8) {
+                                  return 'Password must be at least 8 characters long.';
+                                }
+                                return null;
+                              },
+                              onSaved: (value) {
+                                _enteredPassword = value!;
+                              },
+                              obscureText: true,
                             ),
-                            child: Text(_isLogin ? "Login" : "Sign-up"),
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              setState(() {
-                                _isLogin =
-                                    !_isLogin; // Toggle between login and sign-up modes
-                              });
-                            },
-                            child: Text(_isLogin
-                                ? "Create an account"
-                                : "I already have an account. Login"),
-                          ),
+                          const SizedBox(height: 12),
+                          if (_isAuthenticating)
+                            const CircularProgressIndicator(),
+                          if (!_isAuthenticating)
+                            ElevatedButton(
+                              onPressed:
+                                  _submit, // Call the _submit function on button press
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Theme.of(context)
+                                    .colorScheme
+                                    .primaryContainer,
+                              ),
+                              child: Text(_isLogin ? "Login" : "Sign-up"),
+                            ),
+                          if (!_isAuthenticating)
+                            TextButton(
+                              onPressed: () {
+                                setState(() {
+                                  _isLogin =
+                                      !_isLogin; // Toggle between login and sign-up modes
+                                });
+                              },
+                              child: Text(_isLogin
+                                  ? "Create an account"
+                                  : "I already have an account. Login"),
+                            ),
                         ],
                       ),
                     ),
